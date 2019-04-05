a20c56a0fca8b9fecc81382977ee310a0a4f6ebe
Justin Ryder <ryderj@curse.com>
2019-04-05 16:26:34 -0500

    🐛 Added attribution for the kawaii emoji

--- src/components/EmojiPageFooter.js
+++ src/components/EmojiPageFooter.js
@@ -1,5 +1,10 @@
+import React from 'react';
 
 const EmojiPageFooter = () =>
-    null;
+    <div className="app__footer">
+        <p className="app__footer-message">
+            Kawaii emoji from <a href="http://japaneseemoticons.me/cat-emoticons/" target="_blank" rel="nofollow noreferrer">JapaneseEmoticons.me</a>.
+        </p>
+    </div>;
 
 export default EmojiPageFooter;
