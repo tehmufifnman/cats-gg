9c5ac512d8e4253b448916ef2b99a2a875d82347
Brandon Nelson <brandon.nelson@carvana.com>
2021-09-01 11:49:04 -0700

    update giphy keyword

--- src/utils/giphy/getRandomCatGif.js
+++ src/utils/giphy/getRandomCatGif.js
@@ -2,7 +2,7 @@ import getGiphyApiKey from './getGiphyApiKey';
 
 const createGetRandomCatGif = (apiKey) =>
     () =>
-        fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=cat&rating=g`)
+        fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}&tag=muffin&rating=g`)
             .then(response => response.json())
             .then(json => json.data);
 
